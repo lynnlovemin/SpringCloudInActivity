332403dfd4ca1e9cf9e3d763fae27d36a0ab231c
lynnlovemin <331509758@qq.com>
2018-05-30 19:33:16 +0800

    项目初始导入

---
+++ gateway/src/main/java/com/lynn/gateway/Application.java
@@ -0,0 +1,13 @@
+package com.lynn.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
