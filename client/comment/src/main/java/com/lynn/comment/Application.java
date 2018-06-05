59548dcf80101941266fcf12264ce39aa987045f
lynnlovemin <331509758@qq.com>
2018-06-05 22:15:16 +0800

    集成feign

--- client/comment/src/main/java/com/lynn/comment/Application.java
+++ client/comment/src/main/java/com/lynn/comment/Application.java
@@ -3,9 +3,11 @@ package com.lynn.comment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) {
