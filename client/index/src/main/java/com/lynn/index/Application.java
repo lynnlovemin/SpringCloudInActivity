5cb52fd265598d027c2290324aa2a2388f76aa86
lynnlovemin <331509758@qq.com>
2018-06-21 21:21:07 +0800

    集成druid

--- client/index/src/main/java/com/lynn/index/Application.java
+++ client/index/src/main/java/com/lynn/index/Application.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-//@EnableEncrypt
+@EnableEncrypt
 public class Application {
 
     public static void main(String[] args) {
---
+++ common/src/main/java/com/lynn/common/WebConfig.java
@@ -0,0 +1,47 @@
+//package com.lynn.common;
+//
+//import com.alibaba.fastjson.serializer.SerializerFeature;
+//import com.alibaba.fastjson.support.config.FastJsonConfig;
+//import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+//import org.springframework.boot.SpringBootConfiguration;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@SpringBootConfiguration
+//public class WebConfig extends WebMvcConfigurationSupport {
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        super.configureMessageConverters(converters);
+//        /*
+//        1.需要先定义一个convert转换消息的对象；
+//        2.添加fastjson的配置信息，比如是否要格式化返回的json数据
+//        3.在convert中添加配置信息
+//        4.将convert添加到converters中
+//         */
+//        //1.定义一个convert转换消息对象
+//        FastJsonHttpMessageConverter fastConverter=new FastJsonHttpMessageConverter();
+//        //2.添加fastjson的配置信息，比如：是否要格式化返回json数据
+//        FastJsonConfig fastJsonConfig=new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(
+//                SerializerFeature.PrettyFormat
+//        );
+//        List<MediaType> mediaTypeList = new ArrayList<>();
+//        mediaTypeList.add(MediaType.APPLICATION_JSON_UTF8);
+//        fastConverter.setSupportedMediaTypes(mediaTypeList);
+//        fastConverter.setFastJsonConfig(fastJsonConfig);
+//        converters.add(fastConverter);
+//    }
+//
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        super.addResourceHandlers(registry);
+//        registry.addResourceHandler("**/*.css", "**/*.js", "**/*.map", "*.html", "*.txt", "*.p12").addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(0);
+//    }
+//
+//}
